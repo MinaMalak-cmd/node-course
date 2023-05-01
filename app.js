54d1dc15a83bd10b6605e7c0e6149a86c7cba48a
MinaMalak-cmd <engmina15@yahoo.com>
2023-05-01 18:15:11 +0300

    sharing data using request.body

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminRouter = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 const path = require('path');
 const app = express();
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/admin',adminRouter)
+app.use('/admin', adminData.routes)
 app.use(shopRouter)
 app.use(express.static(path.join(__dirname,'public')));
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,19 @@
 const path = require('path');
 const express = require('express');
-const router = express.Router();
 const rootDir = require('../utils/path');
+const router = express.Router();
 
+const products = ['hello']
 router.get('/add-product',(req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
+router.post('/add-product',(req, res, next) => {
+    products.push(req.body.title);
+    res.redirect('/');
+});
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
+
 
