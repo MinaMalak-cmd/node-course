3eadd2c29c86ce1a22942e4cd5c47f3e97fdf71d
MinaMalak-cmd <engmina15@yahoo.com>
2023-05-20 14:30:20 +0300

    add views

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRouter = require('./routes/shop');
@@ -8,12 +7,8 @@ const screensRouter = require('./routes/screens');
 const path = require('path');
 const app = express();
 
-app.engine('hbs', expressHbs({ 
-    layoutsDir : 'views/layouts', 
-    defaultLayout : 'main-layout',
-    extname: 'hbs' 
-}));
-app.set('view engine', 'hbs');
+
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,8 +19,7 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.use((req, res, next) => {
     res.status(404).render('404', {
-        pageTitle : 'Page Not Found',
-        layout : false
+        pageTitle : 'Page Not Found'
     });
 }); // 404 page
 app.listen(3005); 
