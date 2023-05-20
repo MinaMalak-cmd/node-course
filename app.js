2b89e959921d41c8791cca13f3e02d239f80ff58
MinaMalak-cmd <engmina15@yahoo.com>
2023-05-20 11:07:13 +0300

    add 404 hbs file

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
+
 const adminData = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 const screensRouter = require('./routes/screens');
 const path = require('path');
 const app = express();
 
-// app.set('title', 'My Site')
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,7 +19,6 @@ app.use(screensRouter);
 app.use(express.static(path.join(__dirname,'public')));
 
 app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-    res.status(404).render('404');
+    res.status(404).render('404', {pageTitle : 'Page Not Found'});
 }); // 404 page
 app.listen(3005); 
