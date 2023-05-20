ef811423983379c874fed25633d6c4096ebb7522
MinaMalak-cmd <engmina15@yahoo.com>
2023-05-20 11:37:44 +0300

    remove extra line

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,6 @@ const app = express();
 
 const products = []
 router.get('/add-product',(req, res, next) => {
-    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
     res.render('add-product', {pageTitle: 'Add Product23'})
 });
 
