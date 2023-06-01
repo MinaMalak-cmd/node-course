dacd56ae3ffc7a1ee55809134dae9374f6ccd5eb
MinaMalak-cmd <engmina15@yahoo.com>
2023-06-01 18:23:39 +0300

    add new routes

--- routes/shopRoute.js
+++ routes/shopRoute.js
@@ -6,5 +6,8 @@ const shopController = require('../Controllers/Shop/ShopController');
 router.get('/cart', shopController.addToCart);
 router.get('/products', shopController.getAllProducts);
 router.get('/',  productController.getAllProducts);
+router.get('/products');
+router.get('/cart');
+router.get('/checkout');
 
 module.exports  = router;
