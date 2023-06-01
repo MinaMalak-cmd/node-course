01c61b3c260c088c6bd78bdbfe78b3da678e9ebb
MinaMalak-cmd <engmina15@yahoo.com>
2023-06-01 18:43:19 +0300

    remove unused

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/adminRoute');
-const shopRouter = require('./routes/shopRoute');
-const ErrorController = require('./Controllers/Default/404');
-const path = require('path');
 const app = express();
-
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/admin', adminRoutes)
-app.use(shopRouter);
-app.use(express.static(path.join(__dirname,'public')));
+app.use('/', (req,res,err)=>{
+    res.send('<h1>Welcome!</h1>');
+})
 
-app.use(ErrorController.get404); // 404 page
 app.listen(3005); 
